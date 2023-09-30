740fd6cefdb0f3ed77d8c1ffa3db7dd5e8ce6d95
Ben Brostoff <ben.brostoff@gmail.com>
2023-09-30 13:36:39 -0400

    Begin to add shortcut you fucksticks

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,21 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  console.log('Now sending message to ' + JSON.stringify(tabs[0]));
-  chrome.tabs.sendMessage(tabs[0].id,
-    {
-      message: "copyText",
-      textToCopy: "some text"
-    }, function(response) {
-      console.log(response);
-      console.log('Message was sent.');
-      return true;
-    })
-})
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   console.log('Now sending message to ' + JSON.stringify(tabs[0]));
+//   chrome.tabs.sendMessage(tabs[0].id,
+//     {
+//       message: "copyText",
+//       textToCopy: "some text"
+//     }, function(response) {
+//       console.log(response);
+//       console.log('Message was sent.');
+//       return true;
+//     })
+// })
+
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "my-custom-shortcut") {
+    // Perform the action associated with the shortcut
+    console.log("Custom keyboard shortcut activated!");
+
+    // You can perform any action here, such as opening a popup or executing specific code.
+  }
+});
