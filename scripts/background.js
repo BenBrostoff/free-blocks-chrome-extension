0c5999ca81377b52d988e7984dd9cb2cfa2c7662
Ben Brostoff <ben.brostoff@gmail.com>
2023-10-01 11:32:56 -0400

    Getting real close here

--- scripts/background.js
+++ scripts/background.js
@@ -1,21 +1,13 @@
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   console.log('Now sending message to ' + JSON.stringify(tabs[0]));
-//   chrome.tabs.sendMessage(tabs[0].id,
-//     {
-//       message: "copyText",
-//       textToCopy: "some text"
-//     }, function(response) {
-//       console.log(response);
-//       console.log('Message was sent.');
-//       return true;
-//     })
-// })
+console.log('Initialized');
 
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === "my-custom-shortcut") {
-    // Perform the action associated with the shortcut
-    console.log("Custom keyboard shortcut activated!");
+chrome.runtime.onInstalled.addListener(() => {
+  // TODO - add Klaviyo event here.
+});
 
-    // You can perform any action here, such as opening a popup or executing specific code.
-  }
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.tabs.sendMessage(tab.id,
+    {
+      message: "copyText",
+      textToCopy: "some text"
+    }, function(response) {})
 });
