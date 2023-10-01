ee9ac59679cf09660e958956048ee9f8fba5ec3c
Ben Brostoff <ben.brostoff@gmail.com>
2023-10-01 17:02:11 -0400

    Getting real fuckign closer here JIMM

--- scripts/input.js
+++ scripts/input.js
@@ -1,31 +1,46 @@
 console.log('Originally input loaded.');
 const sourceInput = document.getElementById('calendar-url');
 const buttonInput = document.getElementById('free-times-button');
+const dataHolder = {};
 
-const inputHandler = function(e) {
+const retrieveData = async (slug) => {
+  const toCall = `https://www.freeblockscal.com/api/free-times-from-slug/${slug}`;
+  console.log(`Calling ${toCall}`);
+  //
+  const response = await fetch(toCall);
+  const resJSON = await response.json();
+  // console.log(resJSON);
+  const freeTimes = JSON.stringify(resJSON.free_times);
+  console.log(freeTimes);
+  dataHolder.hiddenContent = freeTimes;
+  // console.log('Copied', freeTimes);
+}
+
+const populate = async () => {
+  const data = await chrome.storage.sync.get();
+  const slug = data.calendar_slug;
+  if (slug) {
+    sourceInput.value = slug;
+    await retrieveData(slug);
+  }
+}
+
+populate();
+
+const inputHandler = async function(e) {
   const val = e.target.value;
-  chrome.storage.sync.set({ calendar_slug: val });
+  const slugValue = await chrome.storage.sync.set('calendar_slug');
+  console.log('Set', slugValue);
 }
 
 const copyFreeTimes = async function(e) {
-  const slugData = await chrome.storage.sync.get(['calendar_slug']);
-  const slug = slugData.calendar_slug;
 
-  const toCall = `https://www.freeblockscal.com/api/free-times-from-slug/${slug}`;
-  console.log(`Calling ${toCall}`);
-
-  const response = await fetch(toCall);
-  const resJSON = await response.json();
-  console.log(resJSON);
-  const freeTimes = resJSON.free_times;
-  console.log('Copied', freeTimes);
-
-  navigator.clipboard.writeText('here we go fucker').then(() => {
+  navigator.clipboard.writeText(dataHolder.hiddenContent).then(() => {
     //clipboard successfully set
-    console.log('worked')
+    console.log('worked');
   }, (e) => {
     //clipboard write failed, use fallback
-    console.log('fauled', e);
+    console.log('failed', e);
   });
 }
 
