aa8cd70b30146a3f09beaee96c1f25a9fd80d757
Ben Brostoff <ben.brostoff@gmail.com>
2023-10-01 11:42:32 -0400

    We are passing messages aroudn yall

--- scripts/background.js
+++ scripts/background.js
@@ -2,12 +2,17 @@ console.log('Initialized');
 
 chrome.runtime.onInstalled.addListener(() => {
   // TODO - add Klaviyo event here.
+
+  // Brostoff set
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.storage.sync.set({ calendar_url: 'https://freeblockscal.com/book/ben-brostoff'});
+
+  const calendarURL = await chrome.storage.sync.get(['calendar_url']);
   chrome.tabs.sendMessage(tab.id,
     {
       message: "copyText",
-      textToCopy: "some text"
+      textToCopy: calendarURL,
     }, function(response) {})
 });
