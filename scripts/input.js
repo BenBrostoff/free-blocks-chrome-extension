25873647e6c601759e11a8015010f185b32a011f
Ben Brostoff <ben.brostoff@gmail.com>
2023-10-01 17:49:42 -0400

    Format dem times baby. FORMAT DEM TIMES

--- scripts/input.js
+++ scripts/input.js
@@ -3,6 +3,52 @@ const sourceInput = document.getElementById('calendar-url');
 const buttonInput = document.getElementById('free-times-button');
 const dataHolder = {};
 
+function convertToFormattedString(events) {
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const formattedEvents = events.map(event => {
+    const startTime = new Date(event.start);
+    const endTime = new Date(event.end);
+
+    const weekday = weekdays[startTime.getUTCDay()];
+    const startHour = startTime.getUTCHours();
+    const startMinutes = startTime.getUTCMinutes();
+    const endHour = endTime.getUTCHours();
+    const endMinutes = endTime.getUTCMinutes();
+
+    const startTimeString = `${startHour}:${startMinutes.toString().padStart(2, '0')}`;
+    const endTimeString = `${endHour}:${endMinutes.toString().padStart(2, '0')}`;
+
+    return `${weekday}, ${new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric' }).format(startTime)}, ${startTimeString}${startHour >= 12 ? 'PM' : 'AM'}-${endTimeString}${endHour >= 12 ? 'PM' : 'AM'}`;
+  });
+
+  return formattedEvents;
+}
+
+function convertToFormattedString(events) {
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const formattedEvents = events.map(event => {
+    const startTime = new Date(event.start);
+    const endTime = new Date(event.end);
+
+    const weekday = weekdays[startTime.getUTCDay()];
+    const startHour = startTime.getUTCHours() % 12 || 12; // Convert to 12-hour format
+    const startMinutes = startTime.getUTCMinutes();
+    const endHour = endTime.getUTCHours() % 12 || 12; // Convert to 12-hour format
+    const endMinutes = endTime.getUTCMinutes();
+    const ampmStart = startTime.getUTCHours() >= 12 ? 'PM' : 'AM';
+    const ampmEnd = endTime.getUTCHours() >= 12 ? 'PM' : 'AM';
+
+    const startTimeString = `${startHour}:${startMinutes.toString().padStart(2, '0')}${ampmStart}`;
+    const endTimeString = `${endHour}:${endMinutes.toString().padStart(2, '0')}${ampmEnd}`;
+
+    return `${weekday}, ${new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric' }).format(startTime)}, ${startTimeString} - ${endTimeString}`;
+  });
+
+  return formattedEvents;
+}
+
 const retrieveData = async (slug) => {
   const toCall = `https://www.freeblockscal.com/api/free-times-from-slug/${slug}`;
   console.log(`Calling ${toCall}`);
@@ -10,9 +56,14 @@ const retrieveData = async (slug) => {
   const response = await fetch(toCall);
   const resJSON = await response.json();
   // console.log(resJSON);
-  const freeTimes = JSON.stringify(resJSON.free_times);
-  console.log(freeTimes);
-  dataHolder.hiddenContent = freeTimes;
+  const freeTimes = resJSON.free_times;
+  const formatted = convertToFormattedString(freeTimes);
+  let str = '';
+  formatted.forEach(dt => {
+    str +=`\n - ${dt}`;
+  })
+
+  dataHolder.hiddenContent = str;
   // console.log('Copied', freeTimes);
 }
 
@@ -43,6 +94,7 @@ const copyFreeTimes = async function(e) {
     console.log('failed', e);
   });
 }
+
 
 sourceInput.addEventListener('input', inputHandler);
 buttonInput.addEventListener('click', copyFreeTimes);
